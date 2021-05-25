6074060c1f8c955a9d346ee62c2313900b1cf975
abhijit-hota <abhihota025@gmail.com>
2021-05-25 17:34:19 +0530

    fix: document title warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "../common/header";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
 	return (
 		<ChakraProvider>
+			<Head>
+				<title>Cards against Humanity</title>
+				<meta name="description" content="A Private CAH client" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Header />
 			<Component {...pageProps} />
 		</ChakraProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,11 @@
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Main, NextScript } from "next/document";
 import theme from "../utils/theme";
 
 export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<title>Cards against Humanity</title>
-					<meta name="description" content="A Private CAH client" />
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
 				<body>
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 					<Main />
