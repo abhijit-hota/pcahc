71aa3652f67f7967f4f2a2a834582840b7ab4d80
abhijit-hota <abhihota025@gmail.com>
2021-05-25 16:38:08 +0530

    fix: locally duplicated cards

--- utils/getRandomCard.js
+++ utils/getRandomCard.js
@@ -5,9 +5,22 @@ const NUM_WHITES = whites.length;
 const NUM_BLACKS = blacks.length;
 
 const getRandomBlackCard = () => Math.floor(Math.random() * NUM_BLACKS);
-const getRandomWhiteCards = (n = 6) =>
-	Array(n)
-		.fill(0)
-        .map(() => Math.floor(Math.random() * NUM_WHITES));
-        
+
+const getRandomWhiteCards = (n = 6) => {
+	const _n = Math.min(10, n);
+
+	const result = [];
+	for (let i = 0; i < _n; i++) {
+		const getUniqueRandomNumber = () => {
+			const rand = Math.floor(Math.random() * NUM_WHITES);
+			if (result.includes(rand)) {
+				return getUniqueRandomNumber();
+			}
+			return rand;
+		};
+		result.push(getUniqueRandomNumber());
+	}
+	return result;
+};
+
 export { getRandomBlackCard, getRandomWhiteCards };
