6ce1b84da138b6521b656a3401975f8c42daa84d
abhijit-hota <abhihota025@gmail.com>
2021-05-26 18:34:48 +0530

    add: readme and separate out fb config

--- utils/_firebase.js
+++ utils/_firebase.js
@@ -1,16 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/database";
+import firebaseConfig from "./firebaseConfig";
 
 if (firebase.apps.length === 0) {
-	const firebaseConfig = {
-		apiKey: "AIzaSyCLQW1G3Qob1wHSDfv-K72aFC6HlWk4yIs",
-		authDomain: "pcahc-9e856.firebaseapp.com",
-		databaseURL: "https://pcahc-9e856-default-rtdb.asia-southeast1.firebasedatabase.app/",
-		projectId: "pcahc-9e856",
-		storageBucket: "pcahc-9e856.appspot.com",
-		messagingSenderId: "480285519283",
-		appId: "1:480285519283:web:ecf97ba64de8d76c37afc6",
-	};
 	firebase.initializeApp(firebaseConfig);
 }
 export const db = firebase.database();
---
+++ utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCLQW1G3Qob1wHSDfv-K72aFC6HlWk4yIs",
+	authDomain: "pcahc-9e856.firebaseapp.com",
+	databaseURL: "https://pcahc-9e856-default-rtdb.asia-southeast1.firebasedatabase.app/",
+	projectId: "pcahc-9e856",
+	storageBucket: "pcahc-9e856.appspot.com",
+	messagingSenderId: "480285519283",
+	appId: "1:480285519283:web:ecf97ba64de8d76c37afc6",
+};
+export default firebaseConfig;
