b5351852058c54d9850435596d039af217650fab
abhijit-hota <abhihota025@gmail.com>
2021-05-26 19:26:29 +0530

    fix: envs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+/* eslint-disable */
+
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
+
+module.exports = (phase) => ({
+	env: {
+		DEPLOYMENT_ENV:
+			phase === PHASE_DEVELOPMENT_SERVER ? "development" : phase === PHASE_PRODUCTION_BUILD ? "production" : "",
+	},
 	images: {
 		domains: ["avatar.tobi.sh"],
 	},
-};
+});
